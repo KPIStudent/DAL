8e2f7a37430dcf155e7b6e10fab1f2945f128813
VovaSheliag <polar.caje@gmail.com>
2021-12-01 06:47:55 +0200

    Added models

--- air/main/models.py
+++ air/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Chronology(models.Model):
+    last_update = models.DateField()
+    last_post = models.DateField()
+    post_periodicity = models.IntegerField()
+    update_periodicity = models.IntegerField()
+
+
+class Station(models.Model):
+    region = models.CharField(max_length=64)
+    pollution_percentage = models.DecimalField()
+    allowable_rate = models.DecimalField()
+    chronology = models.ForeignKey(Chronology, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.region} | {self.pollution_percentage}'
